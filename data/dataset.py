698599cf8394665dcc4f7277f7f70465c107b071
asroman <asroman@ucdavis.edu>
2024-09-26 22:21:57 -0700

    add hardcoded rir resampling (tmp fix)

--- data/dataset.py
+++ data/dataset.py
@@ -203,6 +203,7 @@ class MultichannelDataset(Dataset):
         spaital_targets = self.fetch_spatial_targets(reverb_item)
         reverb_path = os.path.join(self.reverb_path_root, self.reverb_type, reverb_item['fname'])
         reverb = torch.from_numpy(np.load(reverb_path)).float()
+        reverb = signal.resample_poly(reverb, 32000, 44100) # [asroman] - hardcoding as the RIRs I extracted are sr=44100
 
         reverb_padding = 32000 * 2 - reverb.shape[1]
         if reverb_padding > 0:
